46144c7a7024c8f1b5caacc3f8942daa3bf97486
Silvio Limeira <sicemal@gmail.com>
2021-07-10 12:00:33 -0300

    Code User Entity, User Repository and Unit Tests

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Role.java
@@ -1,5 +1,7 @@
 package com.shopme.common.entity;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -21,8 +23,14 @@ public class Role {
 	@Column(length = 150, nullable = false)
 	private String description;
 
-	public Role() { }
-	
+	public Role() {
+	}
+
+	public Role(Integer id) {
+		super();
+		this.id = id;
+	}
+
 	public Role(String name) {
 		super();
 		this.name = name;
@@ -56,6 +64,28 @@ public class Role {
 
 	public void setDescription(String description) {
 		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Role other = (Role) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
 	}
 
 }
--- ShopmeCommon/src/main/java/com/shopme/common/entity/Util.java
+++
@@ -1,9 +0,0 @@
-package com.shopme.common.entity;
-
-public class Util {
-
-	public static void main(String[] args) {
-		System.out.println("Hello Common Util...");
-	}
-	
-}
